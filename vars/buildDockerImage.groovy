6b7443c7252758e9993630e790b61cd387377896
pradeev-ssp <pradeev812@gmail.com>
2026-03-10 11:36:27 +0000

    win update

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -4,6 +4,7 @@ def call(String imageName, String imageTag = "latest") {
     echo "========================================="
     echo " Building Docker Image: ${imageName}:${imageTag} "
     echo "========================================="
+    tool mvnHome=tool 'M3'
 
     // Using the Jenkins sh step to run Docker
     bat "docker build -t ${imageName}:${imageTag} ."
